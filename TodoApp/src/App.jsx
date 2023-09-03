0a6d76f8047aab2fb727060bed7b4eb5e00756e9
Pranshu <pranshu2.71828@gmail.com>
2023-09-03 12:13:44 +0530

    Initial commit setting up the app

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,20 @@
+import { React } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="test-3xl font-bold underline">
+          Hello World!
+        </h1>
+        <h1 className="text-black-200 font-light underline bg-zinc-50">
+          Bye
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
