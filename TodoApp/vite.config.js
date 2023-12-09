d39866cea5841e181f3ee0d7dd86d000d517a482
Pranshu <pranshu2.71828@gmail.com>
2023-12-09 17:13:34 +0530

    aa

--- TodoApp/vite.config.js
+++ TodoApp/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-icons',
-        'react-icons/bs',
-        'framer-motion',
-      ],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       'react-icons',
+  //       'react-icons/bs',
+  //       'framer-motion',
+  //     ],
+  //   },
+  // },
 })
